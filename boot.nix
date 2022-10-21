7782a3641ce96f1939c733022c44b413d19fe27f
Takafumi Asano <cariandrum22@gmail.com>
2022-10-21 12:18:12 +0900

    Increase inotify file watches limit

--- boot.nix
+++ boot.nix
@@ -5,9 +5,7 @@
     loader.systemd-boot.enable = true;
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "console=ttyS0,115200" ];
-    kernel.sysctl = {
-      "fs.inotify.max_user_instances" = 8192;
-    };
+    kernel.sysctl = { "fs.inotify.max_user_instances" = 8192; };
     supportedFilesystems = [ "ntfs" ];
   };
 }
