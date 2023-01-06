86e6fa9b19328c501bf31a483899d174a3f24800
Takafumi Asano <cariandrum22@gmail.com>
2023-01-06 18:54:51 +0900

    Some changes were made to the services
    - Change the structure of the services attribute set
    - Enable CUPS and Avahi for IPP Everyware
    - Delete unused xrdp

--- services.nix
+++ services.nix
@@ -1,92 +1,98 @@
 { pkgs, ... }:
 
 {
-  # Configure the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    extraConfig = ''
-      StreamLocalBindUnlink yes
-    '';
-  };
-
-  # Enable the Keybase service.
-  services.keybase.enable = true;
-
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable PC/SC daemon.
-  services.pcscd.enable = true;
-
-  # For YubiKey and Ledger
-  services.udev.packages =
-    [ pkgs.yubikey-personalization pkgs.ledger-udev-rules ];
-
-  # Configure the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us";
-
-    # For natural scrolling
-    libinput = {
+  services = {
+    # Configure the OpenSSH daemon.
+    openssh = {
       enable = true;
-      mouse.naturalScrolling = true;
-    };
-
-    # I'm currently using a nVidia Graphics Card.
-    videoDrivers = [ "nvidia" ];
-
-    # Configure Display Manager.
-    displayManager.lightdm = {
-      enable = true;
-      extraSeatDefaults = ''
-        display-setup-script=${
-          pkgs.writeScript "lightdm-display-setup" ''
-            #!${pkgs.bash}/bin/bash
-            ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 3840x2160 --rate 60.0 --primary --output DP-4 --mode 3840x2160 --rate 60.0 --left-of DP-2
-          ''
-        }
+      extraConfig = ''
+        StreamLocalBindUnlink yes
       '';
     };
 
-    # Configure Window Manager
-    windowManager.xmonad.enable = true;
-    displayManager.defaultSession = "none+xmonad";
-  };
+    # Enable the Keybase service.
+    keybase.enable = true;
 
-  services.dbus.packages = [ pkgs.gnome3.gnome-keyring pkgs.gcr ];
+    # Enable CUPS to print documents.
+    printing= {
+      enable = true;
+      drivers = with pkgs; [ cnijfilter2 ];
+    };
 
-  services.gnome = {
-    at-spi2-core.enable = true;
-    tracker.enable = true;
-  };
+    # Enable Avahi daemon.
+    avahi = {
+      enable = true;
+      nssmdns = true;
+    };
 
-  services.xrdp = {
-    enable = true;
-    defaultWindowManager = "xmonad";
-  };
+    # Enable PC/SC daemon.
+    pcscd.enable = true;
 
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    shares = {
-      TimeMachine = {
-        path = "/mnt/magnetic/disk0/TimeMachine";
-        browseable = "yes";
-        writable = "yes";
-        "vfs objects" = "catia fruit streams_xattr";
-        "fruit:metadata" = "stream";
-        "fruit:encoding" = "native";
-        "fruit:time machine" = "yes";
-        "fruit:time machine max size" = "2T";
+    # For YubiKey and Ledger
+    udev.packages =
+      [ pkgs.yubikey-personalization pkgs.ledger-udev-rules ];
+
+    # Configure the X11 windowing system.
+    xserver = {
+      enable = true;
+      layout = "us";
+
+      # For natural scrolling
+      libinput = {
+        enable = true;
+        mouse.naturalScrolling = true;
       };
-      Photos = {
-        path = "/mnt/magnetic/disk0/Photos";
-        browseable = "yes";
-        writable = "yes";
-        "vfs objects" = "catia fruit streams_xattr";
-        "fruit:metadata" = "stream";
-        "fruit:encoding" = "native";
+
+      # I'm currently using a nVidia Graphics Card.
+      videoDrivers = [ "nvidia" ];
+
+      # Configure Display Manager.
+      displayManager.lightdm = {
+        enable = true;
+        extraSeatDefaults = ''
+          display-setup-script=${
+            pkgs.writeScript "lightdm-display-setup" ''
+              #!${pkgs.bash}/bin/bash
+              ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 3840x2160 --rate 60.0 --primary --output DP-4 --mode 3840x2160 --rate 60.0 --left-of DP-2
+            ''
+          }
+        '';
+      };
+
+      # Configure Window Manager
+      windowManager.xmonad.enable = true;
+      displayManager.defaultSession = "none+xmonad";
+    };
+
+    dbus.packages = [ pkgs.gnome3.gnome-keyring pkgs.gcr ];
+
+    gnome = {
+      at-spi2-core.enable = true;
+      tracker.enable = true;
+    };
+
+    samba = {
+      enable = true;
+      securityType = "user";
+      shares = {
+        TimeMachine = {
+          path = "/mnt/magnetic/disk0/TimeMachine";
+          browseable = "yes";
+          writable = "yes";
+          "vfs objects" = "catia fruit streams_xattr";
+          "fruit:metadata" = "stream";
+          "fruit:encoding" = "native";
+          "fruit:time machine" = "yes";
+          "fruit:time machine max size" = "2T";
+        };
+        Photos = {
+          path = "/mnt/magnetic/disk0/Photos";
+          browseable = "yes";
+          writable = "yes";
+          "vfs objects" = "catia fruit streams_xattr";
+          "fruit:metadata" = "stream";
+          "fruit:encoding" = "native";
+        };
       };
     };
   };
