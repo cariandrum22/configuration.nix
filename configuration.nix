d76fc3d370059c76401b3905302f55188e32c8ad
Takafumi Asano <cariandrum22@gmail.com>
2023-05-29 17:48:33 +0900

    Update system state version from 22.11 to 23.05

--- configuration.nix
+++ configuration.nix
@@ -104,5 +104,5 @@ in
     '';
   };
 
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
 }
--- i18n.nix
+++ i18n.nix
@@ -4,8 +4,10 @@
   # Configure internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    inputMethod.enabled = "fcitx";
-    inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [ fcitx5-mozc fcitx5-gtk ];
+    };
   };
 
   console = {
