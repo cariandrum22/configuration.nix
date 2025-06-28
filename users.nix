2976437f752c500d114a55a4bc8211478fee61af
Takafumi Asano <cariandrum22@gmail.com>
2025-06-28 15:18:58 +0900

    Add SSH public key for Termius

--- users.nix
+++ users.nix
@@ -13,6 +13,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHtv7BugMwASTVv4+FZi3HlSke0cCNogLuTQQVm/aWc"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJ1pRhJvcW/L0Xzc5G+Lapg2OCCssWGi1bGs+9gSuXc07vSqfuTLHX1kJ1JGkGgWcEVUBYpyZ/aiKDPlKxTMdZY="
     ];
   };
 }
